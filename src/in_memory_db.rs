66554e808520ba05efc9a64eecca39b9c34bb637
hubert <hubbertt.k@gmail.com>
2024-07-11 20:38:17 +0200

    feat: implement Storage.insert for InMemoryDb

--- src/in_memory_db.rs
+++ src/in_memory_db.rs
@@ -1,5 +1,26 @@
+use crate::traits::Storage;
 use std::collections::HashMap;
 
 pub struct InMemoryStorage<T> {
     db: HashMap<String, T>,
+}
+
+impl<T> Storage<T> for InMemoryStorage<T> {
+    fn insert(&mut self, key: String, document: T) -> bool {
+        self.db.insert(key, document);
+        return true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert() {
+        let mut storage = InMemoryStorage { db: HashMap::new() };
+        storage.insert("foo".to_string(), "bar");
+        let document = storage.db.get("foo");
+        assert_eq!(document, Some(&"bar"));
+    }
 }
